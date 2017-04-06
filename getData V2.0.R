6d7a3cd19199ee8cc4d54486524ecc8f6368ac69
xn <qcsd@Outlook.com>
2017-04-06 09:39:48 +0800

    no message

--- getData V2.0.R
+++ getData V2.0.R
@@ -36,37 +36,36 @@ beijing_aqi <- fun.tqhb_AQI('beijing',2016,2016)
 #   read temperature data   #
 #############################
 
-temperature <- data.frame()
+temperature_2 <- data.frame()
 fun.tqhb_tem <- function(address,startyear,endyear){
   for(y in startyear:endyear){
-    for(month in 1:12){
+    for(month in 9:9){
       ym <- y*100+month
       URL <-  paste0('http://www.tianqihoubao.com/lishi/',address,'/month/',ym,'.html')
-      temperature_one <- readHTMLTable(URL, encoding = "GBK", stringsAsFactors = F)[[1]]
-      temperature <- rbind(temperature,temperature_one)
+      temperature_1 <- readHTMLTable(URL, encoding = "GBK", stringsAsFactors = F)[[1]]
+      temperature_2 <- rbind(temperature_2,temperature_1)
     }
   }
-   return(temperature)
- }
 
-# 提取最高气温和最低气温
-fun.tem_str <- function(data_in,data_var){
-  a1 <- word(data_var,1)
-  a2 <- word(data_var,81)
+  a1 <- word(temperature_2$气温,1)
+  a2 <- word(temperature_2$气温,81)
   b1 <- str_extract(a1,'-?\\d')
   b2 <- str_extract(a2,'-?\\d')
-  data_in <- data.frame(data_in, b1,b2)
-  return(data_in)
-  # write.csv(data_in,file=paste0(data_in,'-',startyear,'~',endyear,'.csv'),fileEncoding = 'UTF-8')
-}
+  temperature <- data.frame(temperature_2, b1,b2)
+  
+  return(temperature)
+  # write.csv(temperature,file=paste0(address,'_temp-',startyear,'~',endyear,'.csv'),fileEncoding = 'GBK')
+  
+ }
+
 
 # when do not need csv output
 # must use "reture"in function
-beijing_t1 <- fun.tqhb_tem('beijing',2011,2014)  # 2015年数据抓取的表结构不同，无法运行
-beijing_temp <- fun.tem_str(beijing_t,beijing_t$气温)
+beijing_t <- fun.tqhb_tem('beijing',2011,2014)  # 2015年数据抓取的表结构不同，无法运行
+chengdu_t1 <- fun.tqhb_tem('chengdu',2012,2012)  
 
 
 # when need csv output
 # must use "write,csv" in function
-fun.tem_str((beijing_t,beijing_t$气温)) 
-
+fun.tqhb_tem('beijing',2016,2016) 
+fun.tqhb_tem('chengdu',2011,2011)
